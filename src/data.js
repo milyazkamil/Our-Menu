e27260ca498a425e08706fe1605199e2f8196bbd
Milyaz Kamil <milyazkamil@gmail.com>
2023-09-18 21:12:17 +0300

    Created an array containing the information of the menu content

---
+++ src/data.js
@@ -0,0 +1,112 @@
+import image1 from "./assets/images/item-1.jpeg";
+import image2 from "./assets/images/item-2.jpeg";
+import image3 from "./assets/images/item-3.jpeg";
+import image4 from "./assets/images/item-4.jpeg";
+import image5 from "./assets/images/item-5.jpeg";
+import image6 from "./assets/images/item-6.jpeg";
+import image7 from "./assets/images/item-7.jpeg";
+import image8 from "./assets/images/item-8.jpeg";
+import image9 from "./assets/images/item-9.jpeg";
+
+export const buttons = [
+  {
+    id: 1,
+    category: 'all',
+  },
+  {
+    id: 2,
+    category: 'breakfast',
+  },
+  {
+    id: 3,
+    category: 'lunch',
+  },
+  {
+    id: 4,
+    category: 'shakes',
+  },
+];
+
+export const menu = [
+  {
+    id: 1,
+    category: 'breakfast',
+    title: 'Buttermilk Pancakes',
+    image: image1,
+    price: 15.99,
+    description:
+      "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
+  },
+  {
+    id: 2,
+    category: 'lunch',
+    title: 'Diner Double',
+    image: image2,
+    price: 13.99,
+    description:
+      'Vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats.',
+  },
+  {
+    id: 3,
+    category: 'shakes',
+    title: 'Godzilla Milkshake',
+    image: image3,
+    price: 6.99,
+    description:
+      'Ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.',
+  },
+  {
+    id: 4,
+    category: 'breakfast',
+    title: 'Country Delight',
+    image: image4,
+    price: 20.99,
+    description:
+      'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut.',
+  },
+  {
+    id: 5,
+    category: 'lunch',
+    title: 'Egg Attack',
+    image: image5,
+    price: 22.99,
+    description:
+      "Franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up.",
+  },
+  {
+    id: 6,
+    category: 'shakes',
+    title: 'Oreo Dream',
+    image: image6,
+    price: 18.99,
+    description:
+      'Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday.',
+  },
+  {
+    id: 7,
+    category: 'breakfast',
+    title: 'Bacon Overflow',
+    image: image7,
+    price: 8.99,
+    description:
+      'Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird.',
+  },
+  {
+    id: 8,
+    category: 'lunch',
+    title: 'American Classic',
+    image: image8,
+    price: 12.99,
+    description:
+      'On it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut',
+  },
+  {
+    id: 9,
+    category: 'shakes',
+    title: 'Quarantine Buddy',
+    image: image9,
+    price: 16.99,
+    description:
+      'Skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.',
+  },
+];
